60e6592a499c88f08d7a95baec9289d6392532d5
刘鑫 <liuxin@izaodao.org>
2020-02-06 10:01:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 设计模式
+
+## /designPattern/Singleton： 单例模式
+- IdGenerator1.java   饿汉模式
+- IdGenerator2.java   饿汉模式
+- IdGenerator3.java   饿汉模式
+- IdGenerator4.java   饿汉模式
+- IdGenerator5.java   饿汉模式
---
+++ src/main/java/com/fatsnake/designPattern/IdGenerator1.java
@@ -0,0 +1,10 @@
+package com.fatsnake.designPattern;
+
+/**
+ * @Auther: fatsnake
+ * @Description":
+ * @Date:2020-02-06 09:55
+ * Copyright (c) 2020, zaodao All Rights Reserved.
+ */
+public class IdGenerator1 {
+}
